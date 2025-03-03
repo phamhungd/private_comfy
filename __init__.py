5d5aa23016bd8089ad2d46e7835981fc34923f94
phamhungd <phamhung.d@gmail.com>
2025-03-03 14:28:40 +0700

    fix bug

--- __init__.py
+++ __init__.py
@@ -1,25 +1,31 @@
 from functools import wraps
 import json
 from .trim_workflow import PromptTrim
-from .crypto_node import SaveCryptoNode, ExcuteCryptoNode, RandomSeedNode,CryptoCatImage
+from .crypto_node import (
+    SaveCryptoNode,
+    ExcuteCryptoNode,
+    RandomSeedNode,
+    CryptoCatImage,
+)
 
 NODE_CLASS_MAPPINGS = {
     "SaveCryptoNode": SaveCryptoNode,
     "ExcuteCryptoNode": ExcuteCryptoNode,
     "RandomSeedNode": RandomSeedNode,
-    "CryptoCatImage":CryptoCatImage,
+    "CryptoCatImage": CryptoCatImage,
 }
- 
+
+
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "SaveCryptoNode": "🔒 Export Private Workflow",
-    "ExcuteCryptoNode": "🔒 Output",
-    "RandomSeedNode": "🔒 Random Seed",
-    "CryptoCatImage":"🔒 Save Image",
+    "SaveCryptoNode": "🔐 Export Workflow",
+    "ExcuteCryptoNode": "🔑 Load Private",
+    "RandomSeedNode": "🔒 Random",
+    "CryptoCatImage": "🔒 Save Image",
 }
 
 WEB_DIRECTORY = "./js"
 
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAMES_MAPPINGS', 'WEB_DIRECTORY']
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAMES_MAPPINGS", "WEB_DIRECTORY"]
 
 
 from server import PromptServer
@@ -28,17 +34,20 @@ from server import PromptServer
 method_name = "trigger_on_prompt"
 original_method = getattr(PromptServer, method_name)
 
+
 @wraps(original_method)
 def new_trigger_on_prompt(self, json_data):
     prompt = json_data["prompt"]
-    pr = PromptTrim(prompt)    
+    pr = PromptTrim(prompt)
     if pr.has_crypto_node():
         print("has crypto node")
         json_data["prompt"] = pr.replace_prompt()
+
         if "extra_data" in json_data:
             json_data["extra_data"] = {}
             print("hook new_trigger_on_prompt delete extra_data")
-    result = original_method(self, json_data)    
+    result = original_method(self, json_data)
     return result
+
 
 setattr(PromptServer, method_name, new_trigger_on_prompt)
--- crypto_node.py
+++ crypto_node.py
@@ -5,145 +5,149 @@ import torch
 
 from nodes import SaveImage
 from .trim_workflow import PromptTrim, WorkflowTrim
-from.file_compressor import FileCompressor
+from .file_compressor import FileCompressor
 import folder_paths
 
 
-class SaveCryptoNode():
+class SaveCryptoNode:
     def __init__(self):
         pass
 
     @classmethod
     def INPUT_TYPES(s):
         return {
-                "required": {           
-                "workflow_folder": ("STRING", {"default": folder_paths.output_directory}),
-                "workflow_name": ("STRING", {"default": "my_cat.json"}),    
-                "output_images" : ("IMAGE",),                    
-                },
-                "optional": {
-                    "input_anything" : ("*",),
-                },
-                "hidden": {
-                    "unique_id": "UNIQUE_ID",
-                    "prompt": "PROMPT", 
-                    "extra_pnginfo": "EXTRA_PNGINFO",
-                }
-            }    
- 
-    RETURN_TYPES = () 
+            "required": {
+                "export_folder": ("STRING", {"default": folder_paths.output_directory}),
+                "workflow_name": ("STRING", {"default": "my_cat.json"}),
+                "output_images": ("IMAGE",),
+            },
+            "optional": {
+                "input_anything": ("*",),
+            },
+            "hidden": {
+                "unique_id": "UNIQUE_ID",
+                "prompt": "PROMPT",
+                "extra_pnginfo": "EXTRA_PNGINFO",
+            },
+        }
+
+    RETURN_TYPES = ()
     OUTPUT_NODE = True
-    FUNCTION = "crypto"  
-    CATEGORY = "📂 SDVN/🔑 Private"   
- 
-    def crypto(self, workflow_folder, workflow_name, output_images, **kwargs):
-        if not workflow_name or len(workflow_folder) < 2 or len(workflow_name) == 0:
-            raise Exception("CryptoCat folder and filename must be at least two characters long")
-        
-    
-        unique_id = kwargs.pop('unique_id', None)
-        prompt = kwargs.pop('prompt', None)
-        extra_pnginfo = kwargs.pop('extra_pnginfo', None)
+    FUNCTION = "crypto"
+    CATEGORY = "📂 SDVN/🔑 Private"
+
+    def crypto(self, export_folder, workflow_name, output_images, **kwargs):
+        if not workflow_name or len(export_folder) < 2 or len(workflow_name) == 0:
+            raise Exception(
+                "CryptoCat folder and filename must be at least two characters long"
+            )
+
+        unique_id = kwargs.pop("unique_id", None)
+        prompt = kwargs.pop("prompt", None)
+        extra_pnginfo = kwargs.pop("extra_pnginfo", None)
 
         if unique_id is None:
             raise Exception("Warning: 'unique_id' is missing.")
         if prompt is None:
             raise Exception("Warning: 'prompt' is missing.")
 
-        inputs = list(kwargs.values())               
+        inputs = list(kwargs.values())
 
-        temp_dir = os.environ.get('TEMP') or os.environ.get('TMP') or '/tmp'
+        temp_dir = os.environ.get("TEMP") or os.environ.get("TMP") or "/tmp"
         project_temp_folder = os.path.join(temp_dir, workflow_name)
         if not os.path.exists(project_temp_folder):
             os.makedirs(project_temp_folder)
 
-
-        with open(os.path.join(project_temp_folder, "prompt.json"), "w", encoding="utf-8") as f:            
+        with open(
+            os.path.join(project_temp_folder, "prompt.json"), "w", encoding="utf-8"
+        ) as f:
             f.write(json.dumps(prompt, indent=4, ensure_ascii=False))
-        with open(os.path.join(project_temp_folder, "workflow.json"), "w", encoding="utf-8") as f:            
-            f.write(json.dumps(extra_pnginfo, indent=4, ensure_ascii=False))  
+        with open(
+            os.path.join(project_temp_folder, "workflow.json"), "w", encoding="utf-8"
+        ) as f:
+            f.write(json.dumps(extra_pnginfo, indent=4, ensure_ascii=False))
 
-        project_folder = os.path.join(workflow_folder, workflow_name)
+        project_folder = os.path.join(export_folder, workflow_name)
         if not os.path.exists(project_folder):
             os.makedirs(project_folder)
 
-        hide_prompt_path = os.path.join(project_folder, "prompt.dat")        
+        hide_prompt_path = os.path.join(project_folder, "prompt.dat")
+
         wt = WorkflowTrim(extra_pnginfo)
         wt.trim_workflow()
         show_workflow = wt.replace_workflow(hide_prompt_path)
-        with open(os.path.join(project_folder, "workflow.json"), "w", encoding="utf-8") as f:            
+        with open(
+            os.path.join(project_folder, "workflow.json"), "w", encoding="utf-8"
+        ) as f:
             f.write(json.dumps(show_workflow, indent=4, ensure_ascii=False))
 
         pr = PromptTrim(prompt)
-        show_part_prompt,  hide_part_prompt = pr.split_prompt(wt.get_remaining_node_ids())
-        with open(os.path.join(project_temp_folder, "prompt_show.json"), "w", encoding="utf-8") as f:            
-            f.write(json.dumps(show_part_prompt, indent=4, ensure_ascii=False))        
+        show_part_prompt, hide_part_prompt = pr.split_prompt(
+            wt.get_remaining_node_ids()
+        )
+        with open(
+            os.path.join(project_temp_folder, "prompt_show.json"), "w", encoding="utf-8"
+        ) as f:
+            f.write(json.dumps(show_part_prompt, indent=4, ensure_ascii=False))
 
-        FileCompressor.compress_to_json(hide_part_prompt, hide_prompt_path, "19040822")
+        FileCompressor.compress_to_json(hide_part_prompt, hide_prompt_path, "sdvn")
         return (hide_part_prompt,)
 
 
-
-class ExcuteCryptoNode():
+class ExcuteCryptoNode:
     def __init__(self):
         pass
-    
+
     @classmethod
     def INPUT_TYPES(s):
         return {
-            "required": { 
-                "dat_path": ("STRING", {"default": folder_paths.output_directory}),         
+            "required": {
+                "dat_path": ("STRING", {"default": folder_paths.output_directory}),
             },
             "optional": {
-                "input_anything" : ("*",),
+                "input_anything": ("*",),
             },
             "hidden": {
                 "unique_id": "UNIQUE_ID",
-                "prompt": "PROMPT", 
+                "prompt": "PROMPT",
                 "extra_pnginfo": "EXTRA_PNGINFO",
-            }
+            },
         }
- 
-    RETURN_TYPES = ("IMAGE",) 
-    FUNCTION = "excute" 
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "excute"
     CATEGORY = "📂 SDVN/🔑 Private"
- 
+
     def excute(self, **kwargs):
         batch_size = 1
         height = 1024
         width = 1024
-        color = 0xFF0000        
+        color = 0xFF0000
         r = torch.full([batch_size, height, width, 1], ((color >> 16) & 0xFF) / 0xFF)
         g = torch.full([batch_size, height, width, 1], ((color >> 8) & 0xFF) / 0xFF)
         b = torch.full([batch_size, height, width, 1], ((color) & 0xFF) / 0xFF)
-        return (torch.cat((r, g, b), dim=-1), )
-    
+        return (torch.cat((r, g, b), dim=-1),)
 
-class RandomSeedNode():
+
+class RandomSeedNode:
     def __init__(self):
         pass
-    
+
     @classmethod
     def INPUT_TYPES(s):
-        return {
-            "required": {     
-            },
-            "optional": {
-            },
-            "hidden": {
-            }
-        }
- 
-    RETURN_TYPES = ("INT",) 
-    FUNCTION = "random" 
+        return {"required": {}, "optional": {}, "hidden": {}}
+
+    RETURN_TYPES = ("INT",)
+    FUNCTION = "random"
     CATEGORY = "📂 SDVN/🔑 Private"
 
     def IS_CHANGED():
         return float("NaN")
- 
+
     def random(self):
-        return (random.randint(0, 999999), )
-    
+
+        return (random.randint(0, 999999),)
+
 
 class CryptoCatImage(SaveImage):
     def __init__(self):
@@ -154,11 +158,15 @@ class CryptoCatImage(SaveImage):
         return {
             "required": {
                 "images": ("IMAGE", {"tooltip": "The images to save."}),
-                "filename_prefix": ("STRING", {"default": "ComfyUI", "tooltip": "The prefix for the file to save. This may include formatting information such as %date:yyyy-MM-dd% or %Empty Latent Image.width% to include values from nodes."})
+                "filename_prefix": (
+                    "STRING",
+                    {
+                        "default": "ComfyUI",
+                        "tooltip": "The prefix for the file to save. This may include formatting information such as %date:yyyy-MM-dd% or %Empty Latent Image.width% to include values from nodes.",
+                    },
+                ),
             },
-            "hidden": {
-                "prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"
-            },
+            "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
         }
 
     RETURN_TYPES = ()
@@ -169,5 +177,7 @@ class CryptoCatImage(SaveImage):
     CATEGORY = "📂 SDVN/🔑 Private"
     DESCRIPTION = "Saves the input images to your ComfyUI output directory."
 
-    def save_images(self, images, filename_prefix="ComfyUI", prompt=None, extra_pnginfo=None):
+    def save_images(
+        self, images, filename_prefix="ComfyUI", prompt=None, extra_pnginfo=None
+    ):
         return super().save_images(images, filename_prefix, None, None)
